3d3df14cb95a5ee58e9e0ebe910e7bb59da021fa
Angel Abellanosa <auabellanosa@hotmail.com>
2015-11-20 11:37:58 +0800

    Initial commit

---
+++ app/src/main/java/ph/edu/dlsjbc/dodoabellanosa/photogallery/SingleFragmentActivity.java
@@ -0,0 +1,36 @@
+package ph.edu.dlsjbc.dodoabellanosa.photogallery;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Dodo Abellanosa on 11/9/2015.
+ */
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+    @LayoutRes  //annotation to let AS double check return type is a layout
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
+
+}
